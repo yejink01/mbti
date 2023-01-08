d64b7fc8975e609e0fad57d7519430d9643529c4
yejin0604 <104920062+hovg33399@users.noreply.github.com>
2023-01-08 17:07:49 +0900

    Add files via upload

---
+++ mbti/assets/mbti/js/questions.js
@@ -0,0 +1,43 @@
+import { questions } from './data.js'
+
+const progressValueEl = document.querySelector('.progress .value')
+const numberEl = document.querySelector('.number')
+const questionEl = document.querySelector('.question')
+const choice1El = document.querySelector('.choice1')
+const choice2El = document.querySelector('.choice2')
+
+let currentNumber = 0
+let mbti = ''
+
+function renderQuestion() {
+  const question = questions[currentNumber]
+  questionEl.innerHTML = question.question
+  numberEl.innerHTML=question.number
+  choice1El.innerHTML = question.choices[0].text
+  choice2El.innerHTML = question.choices[1].text
+  progressValueEl.style.width = (currentNumber +1) * 10 +'%'
+}
+function nextQuestion(choiceNumber){
+  if (currentNumber === questions.length -1){
+    showResultPage()
+    return
+  }
+  const question = questions[currentNumber]
+  mbti = mbti + question.choices[choiceNumber].value
+  currentNumber = currentNumber + 1
+  renderQuestion()
+}
+
+function showResultPage(){
+  //새 페이지
+  location.href = '/results.html?mbti =' + mbti //쿼리스트링
+
+}
+
+choice1El.addEventListener('click',function(){
+  nextQuestion(0)
+})
+choice2El.addEventListener('click', function(){
+  nextQuestion(1)
+})
+renderQuestion()
